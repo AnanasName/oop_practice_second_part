e46e5e3510350dd8efb3178bb0f2377f7c03ca11
AnanasName <martin25102002@gmail.com>
2023-05-09 00:01:00 +0300

    Tic Tac Toe game added

--- tic_tac_toe_prog.py
+++ tic_tac_toe_prog.py
@@ -1,33 +1,44 @@
 import random
 
 
+# Класс, отражающий сущность клетки игры (содержит свой номер - __number, булево значение занята она или нет - __taken,
+# булево значение, каким игроком она занята - __taken_by_first_player)
 class Cell:
+
+    # Метод инициализации клетки игрового поля
     def __init__(self, taken=False, taken_by_first_player=False, number: int = 0):
         self.__taken = taken
         self.__taken_by_first_player = taken_by_first_player
         self.__number = number
 
+    # Метод получения статуса занятия клетки
     def is_taken(self):
         return self.__taken
 
+    #
     def is_taken_by_first_player(self):
         return self.__taken_by_first_player
 
+    # Метод получения номера клетки
     def get_number(self):
         return self.__number
 
+    # Метод занятия клетки
     def take_cell(self, taken_by_first_player):
         self.__taken = True
         self.__taken_by_first_player = taken_by_first_player
 
 
+# Класс, отражающий сущность доски (содержит в себе список клеток на доске - __cells, представленных классом Cell)
 class Board:
 
+    # Метод инициализации обЪекта доски
     def __init__(self):
         self.__cells = list()
         for i in range(1, 10):
             self.__cells.append(Cell(number=i))
 
+    # Метод печати игрового поля
     def print_board(self):
         self.__print_game_row(list(range(1, 4)))
         print("-+-+-")
@@ -37,12 +48,15 @@ class Board:
 
         self.__print_game_row(list(range(7, 10)))
 
+    # Метод получения статуса занятия клетки
     def is_cell_taken(self, index: int):
         return self.__get_cell(index).is_taken()
 
+    # Метод занятия клетки
     def take_cell(self, index: int, taken_by_first_player: bool):
         self.__get_cell(index).take_cell(taken_by_first_player)
 
+    # Метод проверки победы одного из игроков на поле
     def check_if_win(self, is_first_player: bool) -> bool:
         if self.__check_three_in_row([1, 2, 3], is_first_player):
             return True
@@ -62,6 +76,7 @@ class Board:
             return True
         return False
 
+    # Метод определения "трёх в ряд"
     def __check_three_in_row(self, indexes: list[int], is_first_player: bool) -> bool:
         for index in indexes:
             if (not self.__get_cell(index).is_taken_by_first_player() == is_first_player) or not self.is_cell_taken(
@@ -69,14 +84,17 @@ class Board:
                 return False
         return True
 
+    # Метод получения клетки по номеру
     def __get_cell(self, index: int) -> Cell:
         return next(filter(lambda cell: cell.get_number() == index, self.__cells))
 
+    # Метод для печати строки игрового поля
     def __print_game_row(self, indexes: list):
         print(self.__get_symbol_to_print(self.__get_cell(indexes[0])), end="|")
         print(self.__get_symbol_to_print(self.__get_cell(indexes[1])), end="|")
         print(self.__get_symbol_to_print(self.__get_cell(indexes[2])), end=f" {indexes[0]} {indexes[1]} {indexes[2]}\n")
 
+    # Метод получения символа для печати в клетке
     def __get_symbol_to_print(self, cell: Cell) -> str:
         if cell.is_taken() and cell.is_taken_by_first_player():
             return "X"
@@ -86,30 +104,45 @@ class Board:
             return " "
 
 
+# Класс, отражающий сущность игрока (содержит булево значение выиграл игрок или нет - __won, имя игрока - __player_name)
 class Player:
-    def __init__(self, won=False):
-        self.__won = won
 
-    def is_won(self):
+    # Метод инициализации игрока
+    def __init__(self, won=False, player_name="X"):
+        self.__won = won
+        self.__player_name = player_name
+
+    # Метод получения статуса победы игрока
+    def is_won(self) -> bool:
         return self.__won
 
+    # Метод пометки игрока, как выигравшего
     def make_won(self):
         self.__won = True
 
+    # Метод получения имени игрока
+    def get_player_name(self):
+        return self.__player_name
 
+
+# Класс, отражающий сущность игровой механики
 class Game:
+
+    # Метод, отвечащий за инициализацию объекта игры
     def __init__(self, first_player_turn=True):
         self.first_player_turn = first_player_turn
 
+    # Метод, отвечающий за попытку занять клетку. Возвращает false, если клетку нельзя занять
     def try_take_cell(self, game_board: Board, index: int, taken_by_first_player: bool) -> bool:
         if not game_board.is_cell_taken(index):
             game_board.take_cell(index, taken_by_first_player)
             return True
         return False
 
+    # Главный метод игры, отвечающий за игровой процесс
     def main(self):
-        hum_player = Player()
-        ai_player = Player()
+        hum_player = Player(player_name="X")
+        ai_player = Player(player_name="O")
         game_board = Board()
 
         counter = 9
@@ -117,19 +150,19 @@ class Game:
         print("Welcome to tic-tac-toe!")
         game_board.print_board()
 
-        while not hum_player.is_won or not ai_player.is_won or counter > 0:
+        while (not hum_player.is_won()) and (not ai_player.is_won()) and (counter > 0):
             if self.first_player_turn:
-                print("What is X's move? (1-9)")
+                print(f"What is {hum_player.get_player_name()}'s move? (1-9)")
                 cell_number = input()
                 while not self.try_take_cell(game_board, int(cell_number), True):
                     print("This cell already taken! Choose another!")
                     cell_number = input()
                 game_board.print_board()
                 if game_board.check_if_win(True):
-                    hum_player.is_won()
+                    hum_player.make_won()
 
             else:
-                print("What is O's move? (1-9)")
+                print(f"What is {ai_player.get_player_name()}'s move? (1-9)")
                 cell_number = random.randint(1, 9)
                 while not self.try_take_cell(game_board, int(cell_number), False):
                     cell_number = random.randint(1, 9)
@@ -142,9 +175,9 @@ class Game:
             counter = counter - 1
 
         if hum_player.is_won():
-            print("X has won the game!")
+            print(f"{hum_player.get_player_name()} has won the game!")
         elif ai_player.is_won():
-            print("O has won the game!")
+            print(f"{ai_player.get_player_name()} has won the game!")
         else:
             print("Game tied!")
 
